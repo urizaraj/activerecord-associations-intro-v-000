840c1be548dde6d971d7d9051fe31b7dd8f6ae6d
Alexander Urizar <urizaraj@gmail.com>
2018-02-26 15:07:57 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,5 +1,4 @@
 class CreateSongs < ActiveRecord::Migration
-  class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :name 
@@ -7,5 +6,4 @@ class CreateSongs < ActiveRecord::Migration
       t.integer :genre_id
     end
   end
-end
 end
